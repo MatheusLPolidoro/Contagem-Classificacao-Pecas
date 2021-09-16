f62f04906641d9502051697e6d41a243be9247a4
Matheus Polidoro <matheus.polidoro@senaisp.edu.br>
2021-09-16 13:55:23 -0300

    Incluso lógica conforme fluxograma apresentado em aula

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let listaDeEstudantes = ["Matheus", "Lucas", "Pedro"];
 let quantidadeDeEstudantes = listaDeEstudantes.length;
 
-
+// condicional, estrutura de decisão com saída de mensagem
 if (quantidadeDeEstudantes % 2 == 0 && quantidadeDeEstudantes != 0){
     console.log('O número de estudantes é "Par", total de: ' + quantidadeDeEstudantes)
 } else if (quantidadeDeEstudantes == 0){
