d039a7e64beaeae5e80a6aef3904da5df9113c73
Matheus Polidoro <matheus.polidoro@senaisp.edu.br>
2021-09-16 13:53:54 -0300

    Incluso lógica conforme fluxograma apresentado em aula

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-let NomeProprio = "Matheus"
-console.log(NomeProprio)
-const NomeProprioFixo = "Lucas"
-console.log(NomeProprioFixo)
+// definição de variáveis e atribução de valores
+let listaDeEstudantes = ["Matheus", "Lucas", "Pedro"];
+let quantidadeDeEstudantes = listaDeEstudantes.length;
+
+
+if (quantidadeDeEstudantes % 2 == 0 && quantidadeDeEstudantes != 0){
+    console.log('O número de estudantes é "Par", total de: ' + quantidadeDeEstudantes)
+} else if (quantidadeDeEstudantes == 0){
+    console.log("O número de estudantes é Zero.")
+} else {
+    console.log('O número de estudantes é "Ímpar", total de: ' + quantidadeDeEstudantes)
+}
